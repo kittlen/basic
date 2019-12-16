479f7b25685522a8c85d529e45fce5dadd8d6cc2
kittlen <kittlen@foxmail.com>
2019-12-16 15:37:37 +0800

    cherry-pick test

--- MyUtil/src/cherryPick.java
+++ MyUtil/src/cherryPick.java
@@ -3,7 +3,7 @@ public class cherryPick {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("cherry-pick");
+		System.out.println("这是我用来练习cherry-pick的");
 	}
 
 }
